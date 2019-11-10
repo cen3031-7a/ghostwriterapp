ccc1822e428d812746d66b1e16f616ff888bef9f
seising99 <54587646+seising99@users.noreply.github.com>
2019-11-10 15:28:41 -0500

    added build

---
+++ client/build/precache-manifest.d541de53956781f5514e5461d466178d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae905cf6a9eb114207e13ec70dad436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4232295c08e2d58b302c",
+    "url": "/static/css/2.9dcc293d.chunk.css"
+  },
+  {
+    "revision": "5d973371f61288708da0",
+    "url": "/static/css/main.792bc44b.chunk.css"
+  },
+  {
+    "revision": "4232295c08e2d58b302c",
+    "url": "/static/js/2.7bacfa1a.chunk.js"
+  },
+  {
+    "revision": "5d973371f61288708da0",
+    "url": "/static/js/main.2c34037a.chunk.js"
+  },
+  {
+    "revision": "4885bf7e750cced190f9",
+    "url": "/static/js/runtime-main.be1c2073.js"
+  }
+]);
