13dc97bd7d8072320760ff2b2460dd1841944000
Carl Kogstrom <ckogstrom@ufl.edu>
2019-11-16 13:36:30 -0500

    unstylized but temporary toolbar

--- client/src/views/QuestionPage/QuestionPage.js
+++ client/src/views/QuestionPage/QuestionPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import  './QuestionPage.css';
 import Dashboard from '../../components/Dashboard/Dashboard';
+import Toolbar from '../../components/Toolbar/Toolbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class QuestionPage extends Component {
@@ -35,7 +36,11 @@ class QuestionPage extends Component {
 			
 				<div style={{paddingTop: '50px'}} />
 				
-				<div className="toolbar" > Toolbar </div>
+				<div className="toolbar" > 
+					<Toolbar 
+					data={this.props.questions}
+					/>
+				</div>
 				
 				<div style={{width: '20%', float: 'left'}}> <p></p>  </div>
 				
