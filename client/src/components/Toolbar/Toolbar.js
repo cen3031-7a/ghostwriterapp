1a520dc7c74378bcf2e289152d8e5b536e5e071d
Carl Kogstrom <ckogstrom@ufl.edu>
2019-11-16 20:47:33 -0500

    lets see if this works

--- client/src/components/Toolbar/Toolbar.js
+++ client/src/components/Toolbar/Toolbar.js
@@ -36,6 +36,13 @@ class Toolbar extends Component {
         
         return(
             <div className = "toolbar-wrapper">
+            <div className = "HintButton-wrapper">
+                <button className="hintButton" onClick = {() => this.toggleHint()}>I Want A Hint</button>
+            </div>
+            <div className = "hintBox">
+                {this.state.hintShowing && <p>hi</p>}
+            </div>
+            
             <div className="SelectSection-wrapper">
                 <div className="SelectSection-header" onClick = {()=>this.toggleList()}>
                     <b className="SelectSection-header-title">Select Section {listOpen ? <b>^</b>: <b>></b>}</b>
@@ -50,12 +57,7 @@ class Toolbar extends Component {
                     ))}
                 </ul>}
             </div>
-            <div className = "HintButton-wrapper">
-                <button className="hitButton" onClick = {() => this.toggleHint()}>I want a hint</button>
-            </div>
-            <div className = "hintBox">
-                {this.state.hintShowing && <p>hi</p>}
-            </div>
+
             </div>
         )
     }
