713082a61f50834e2aa4748c3cf417f3e7e0797a
Paolo Garcia <paolo.garcia002@gmail.com>
2019-12-01 11:07:30 -0500

    - set up login page

---
+++ client/src/views/Login/Login.js
@@ -0,0 +1,60 @@
+import { React, Component} from 'react'
+
+class Login extends Component {
+
+    constructor(){
+        super()
+        this.state={
+            email: "",
+            password: ""
+        }
+    }
+
+    update(event){
+        this.setState({ [event.target.id]: event.target.value })
+    }
+
+    submit(event){
+        event.preventDefault()
+
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        fetch("/api/users/login", 
+        {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers:
+			{
+				'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.status(200).redirect("/Questions"))
+    }
+
+    render() {
+        return(
+        <div>
+            <form>                
+                <input
+                id="email"
+                placeholder="Email"
+                type="text"
+                onChange={this.update.bind(this)}/>
+                
+                <input
+                id="password"
+                placeholder="Password"
+                type="password"
+                onChange={this.update.bind(this)}/>
+
+                <button type="submit" onSubmit={this.submit.bind(this)}></button>
+            </form>
+        </div>
+        )
+    }
+}
+
+export default Login;
