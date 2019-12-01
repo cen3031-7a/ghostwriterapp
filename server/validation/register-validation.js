a9b5ff6aad3ac38995d3baade914bee2038bf6a5
Paolo Garcia <paolo.garcia002@gmail.com>
2019-12-01 18:14:30 -0500

    validation scripts for login and registering

---
+++ server/validation/register-validation.js
@@ -0,0 +1,27 @@
+// Form validation
+const validator = require("validator");
+
+module.exports.validUser = (user) => {
+    var err_list = {}
+
+    if(validator.isEmpty(user.firstname) || validator.isEmpty(user.lastname)){
+        err_list.name = "Need first and last name."
+    }
+
+    if(validator.isEmpty(user.email) || !validator.isEmail(user.email)){
+        err_list.email = "Need a proper, non empty email."
+    }
+
+    if(validator.isEmpty(user.password) || validator.isEmpty(user.confirm_password)){
+        err_list.empty_password = "Need both password fields"
+    }
+
+    if(!validator.equals(data.password, data.confirm_password)){
+        err_list.not_matching = "Passwords dont match"
+    }
+
+    return {
+        err_list,
+        isValid: isEmpty(err_list)
+    }
+}
